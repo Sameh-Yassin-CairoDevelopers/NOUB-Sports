d748f9e3ebaa7a4598e0711c29eb21132f106b97
Sameh-Yassin-CairoDevelopers <165069856+Sameh-Yassin-CairoDevelopers@users.noreply.github.com>
2025-12-09 17:59:44 -0800

    Update validators.js

---
+++ js/utils/validators.js
@@ -0,0 +1,13 @@
+/*
+ * Filename: js/utils/validators.js
+ * Description: Helper functions to validate user input.
+ */
+
+export const Validators = {
+    isValidName: (name) => {
+        return name && name.length >= 3 && name.length <= 15;
+    },
+    isValidPosition: (pos) => {
+        return ['FWD', 'MID', 'DEF', 'GK'].includes(pos);
+    }
+};
