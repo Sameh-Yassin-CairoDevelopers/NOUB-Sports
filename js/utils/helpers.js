ee75a8a4ae39ea85d71a2d8b5b8a58d0d72b56e5
Sameh-Yassin-CairoDevelopers <165069856+Sameh-Yassin-CairoDevelopers@users.noreply.github.com>
2025-12-09 18:00:03 -0800

    Update helpers.js

---
+++ js/utils/helpers.js
@@ -0,0 +1,13 @@
+/*
+ * Filename: js/utils/helpers.js
+ * Description: General formatting utilities.
+ */
+
+export const Helpers = {
+    formatCurrency: (amount) => {
+        return new Intl.NumberFormat('en-US').format(amount);
+    },
+    formatDate: (dateObj) => {
+        return dateObj.toLocaleDateString('ar-EG');
+    }
+};
