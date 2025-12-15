0a1f1d2c09745c6c5690e458c6e4bd39e7733fd2
Sameh-Yassin-CairoDevelopers <165069856+Sameh-Yassin-CairoDevelopers@users.noreply.github.com>
2025-12-14 20:43:45 -0800

    Update realtime.js

--- js/services/realtime.js
+++ js/services/realtime.js
@@ -1,4 +1,7 @@
 /* Filename: js/services/realtime.js - Handles Supabase Subscriptions */
+
+
 export class RealtimeService {
     subscribeToMatches(zoneId, callback) { console.log("Subscribed"); }
 }
+
